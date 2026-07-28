f23b4d8609c065f581afb08ba3e350a5ab4e73d9
Marcio Faro <marciofaro@outlook.com>
2026-07-28 00:56:53 -0300

    atualizacao via Atualizar_VM.bat

--- Atualizar_VM.bat
+++ Atualizar_VM.bat
@@ -8,7 +8,7 @@ git push origin master
 
 echo.
 echo === 2) DEPLOY NA VM ORACLE ===
-"C:\Program Files\PuTTY\plink.exe" -ssh -pw 2022 ubuntu@168.138.234.37 "cd /home/ubuntu/Jordao-Automatizacao && git pull && pm2 restart jordao-agente && sleep 3 && pm2 logs jordao-agente --lines 10 --nostream"
+"C:\Program Files\PuTTY\plink.exe" -load "pibo_mv1" "cd /home/ubuntu/Jordao-Automatizacao && git pull && pm2 restart jordao-agente && sleep 3 && pm2 logs jordao-agente --lines 10 --nostream"
 
 echo.
 echo === CONCLUIDO ===
